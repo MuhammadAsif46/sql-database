23f3028da4ee902859b90a14a51b0b461061c512
MuhammadAsif46 <asifahmed32c@gmail.com>
2024-12-06 19:00:18 +0500

    add where clause with operator

--- select-commands/index.sql
+++ select-commands/index.sql
@@ -11,6 +11,12 @@ SELECT DISTINCT city FROM student;
 -- WHERE Clause:
 
 SELECT col1, col2 FROM table_name WHERE conditions;
+
 SELECT * FROM student WHERE marks > 80;
+
 SELECT * FROM student WHERE city = "Karachi";
+
 SELECT * FROM student WHERE marks > 80 AND city = "Karachi";
+
+
+-- WHERE Clause:
