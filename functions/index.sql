fef9850555a4fb087194c14a3feea64f04171a89
MuhammadAsif46 <asifahmed32c@gmail.com>
2024-12-09 13:35:40 +0500

    add aggregation functions

---
+++ functions/index.sql
@@ -0,0 +1,20 @@
+-- Aggregate functions: (most popular)
+
+--Get Count name:
+SELECT COUNT(names) FROM student;
+
+--Get maximum marks:
+SELECT MAX(marks) FROM student;
+
+--Get minimum marks:
+SELECT MIN(marks) FROM student;
+
+--Get avergae marks:
+SELECT AVG(marks) FROM student;
+
+-- Get total marks:
+SELECT SUM(marks) FROM student;
+
+
+
+-- Group by clause:
